b2dad4c72f70eff12dc9a1bdbc6a74681a0da56e
Ian W <49083526+Landaman@users.noreply.github.com>
2023-12-02 01:37:05 -0500

    Fixed some broken lint checks

--- packages/site/src/components/results/popover/LineupPopOver.tsx
+++ packages/site/src/components/results/popover/LineupPopOver.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import {Athlete} from "@/types/types.ts";
+import { Athlete } from "@/types/types.ts";
 
 /**
  * Interface representing the props in a result card
  */
 interface LineupCardProps {
-    lineup: Athlete[];
-    isVisible: boolean;
+  lineup: Athlete[];
+  isVisible: boolean;
 }
 
 /**
@@ -15,23 +15,25 @@ interface LineupCardProps {
  * @param props the properties defining the result card
  */
 function LineupPopOver(props: LineupCardProps) {
-    return (
-        <>
-            {props.isVisible && (
-                <div
-                    className={"relative group inline-block"}>
-                    {props.lineup.map((athlete) => {
-                        return (
-                            <div className={"flex flex-row gap-2.5 text-xs"}>
-                                <div>{athlete.seat}</div>
-                                <div>{athlete.name}</div>
-                            </div>
-                        );
-                    })}
-                </div>
-            )}
-        </>
-    );
+  return (
+    <>
+      {props.isVisible && (
+        <div className={"relative group inline-block"}>
+          {props.lineup.map((athlete) => {
+            return (
+              <div
+                key={athlete.seat}
+                className={"flex flex-row gap-2.5 text-xs"}
+              >
+                <div>{athlete.seat}</div>
+                <div>{athlete.name}</div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default LineupPopOver;
