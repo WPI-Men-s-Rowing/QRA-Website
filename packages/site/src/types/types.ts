9da07a3feb8c7d8c04c6c9337544c2e7408f001a
Ian W <49083526+Landaman@users.noreply.github.com>
2023-12-02 01:37:40 -0500

    Prettier pass

--- packages/site/src/types/types.ts
+++ packages/site/src/types/types.ts
@@ -1,66 +1,65 @@
 // Regatta Types
 export interface Regatta {
-    name: string;
-    date: Date;
-    uuid: string;
-    status: RegattaStatus;
-    heats: Heat[];
-    location: string;
+  name: string;
+  date: Date;
+  uuid: string;
+  status: RegattaStatus;
+  heats: Heat[];
+  location: string;
 }
 
 export interface Heat {
-    title: string;
-    startTime: Date;
-    status: HeatStatus;
-    finishOrder: FinishedCrew[];
-    host: string;
+  title: string;
+  startTime: Date;
+  status: HeatStatus;
+  finishOrder: FinishedCrew[];
+  host: string;
 }
 
 export interface FinishedCrew {
-    teamName: string;
-    totalTime: number;
-    lane: number;
-    place: number;
-    margin: number;
-    splitTimes: number[];
-    lineup: Athlete[];
-    penalty?: Penalty;
+  teamName: string;
+  totalTime: number;
+  lane: number;
+  place: number;
+  margin: number;
+  splitTimes: number[];
+  lineup: Athlete[];
+  penalty?: Penalty;
 }
 
 export interface Athlete {
-    name: string;
-    seat: LineupSeat;
+  name: string;
+  seat: LineupSeat;
 }
 
 export interface Penalty {
-    time: number;
-    reason: string;
+  time: number;
+  reason: string;
 }
 
 // Enums
 export enum HeatStatus {
-    UPCOMING = "UPCOMING",
-    ACTIVE = "ACTIVE",
-    UNOFFICIAL = "UNOFFICIAL",
-    OFFICIAL = "OFFICIAL",
-    BREAK = "BREAK",
+  UPCOMING = "UPCOMING",
+  ACTIVE = "ACTIVE",
+  UNOFFICIAL = "UNOFFICIAL",
+  OFFICIAL = "OFFICIAL",
+  BREAK = "BREAK",
 }
 
 export enum RegattaStatus {
-    UPCOMING = "UPCOMING",
-    ACTIVE = "ACTIVE",
-    FINISHED = "FINISHED",
+  UPCOMING = "UPCOMING",
+  ACTIVE = "ACTIVE",
+  FINISHED = "FINISHED",
 }
 
 export enum LineupSeat {
-    COX = "C",
-    STROKE = "S",
-    SEVEN = "7",
-    SIX = "6",
-    FIVE = "5",
-    FOUR = "4",
-    THREE = "3",
-    TWO = "2",
-    BOW = "B",
+  COX = "C",
+  STROKE = "S",
+  SEVEN = "7",
+  SIX = "6",
+  FIVE = "5",
+  FOUR = "4",
+  THREE = "3",
+  TWO = "2",
+  BOW = "B",
 }
-
