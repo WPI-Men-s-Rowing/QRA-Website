5ea7457eedc17d156ae30569260f88ad027e3cdb
Emerson Shatouhy <emersont2003@gmail.com>
2023-12-10 22:56:55 -0500

    Fix
    Signed-off-by: Emerson Shatouhy <emersont2003@gmail.com>

--- packages/site/src/app/dropdown.tsx
+++ packages/site/src/app/dropdown.tsx
@@ -1,79 +1,83 @@
-"use client"
-import {useEffect, useState} from "react";
-import {useRouter} from "next/navigation";
-import Image, {StaticImageData} from "next/image";
+"use client";
 import communityIcon from "@public/icons/navigation/community-icon.svg";
 import homeIcon from "@public/icons/navigation/home-icon.svg";
 import regattasIcon from "@public/icons/navigation/regattas-icon.svg";
+import Image, { StaticImageData } from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface DropdownItem {
-    label: string;
-    path: string;
+  label: string;
+  path: string;
 }
 
 interface DropdownProps {
-    title: string;
-    items: DropdownItem[];
+  title: string;
+  items: DropdownItem[];
 }
 
-const Dropdown: React.FC<DropdownProps> = ({title, items}) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [icon, setIcon] = useState<StaticImageData>(homeIcon as StaticImageData);
-    const router = useRouter();
+// eslint-disable-next-line react/prop-types
+const Dropdown: React.FC<DropdownProps> = ({ title, items }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [icon, setIcon] = useState<StaticImageData>(
+    homeIcon as StaticImageData,
+  );
+  const router = useRouter();
 
-    const handleItemClick = (path: string) => {
-        router.push(path);
-        setIsOpen(false);
-    };
+  const handleItemClick = (path: string) => {
+    router.push(path);
+    setIsOpen(false);
+  };
 
-    useEffect(() => {
-        switch (title) {
-            case "Home":
-                setIcon(homeIcon as StaticImageData);
-                break;
-            case "Regattas":
-                setIcon(regattasIcon as StaticImageData);
-                break;
-            case "Community":
-                setIcon(communityIcon as StaticImageData);
-                break;
-            default:
-                setIcon(homeIcon as StaticImageData);
-                break;
-        }
-    }, []);
+  useEffect(() => {
+    switch (title) {
+      case "Home":
+        setIcon(homeIcon as StaticImageData);
+        break;
+      case "Regattas":
+        setIcon(regattasIcon as StaticImageData);
+        break;
+      case "Community":
+        setIcon(communityIcon as StaticImageData);
+        break;
+      default:
+        setIcon(homeIcon as StaticImageData);
+        break;
+    }
+  }, [title]);
 
-    return (
-        <div className={`relative`}>
-            <button
-                onClick={() => setIsOpen(!isOpen)}
-                onMouseOver={() => setIsOpen(true)}
-                onMouseLeave={() => setIsOpen(false)}
-                className="flex flex-row text-white items-center gap-2 text-opacity-50"
+  return (
+    <div className={`relative`}>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        onMouseOver={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+        className="flex flex-row text-white items-center gap-2 text-opacity-50"
+      >
+        <Image
+          src={icon}
+          alt={title + " Icon"}
+          className="lg:w-[40px] lg:h-[40px] w-[20px] h-[20px]"
+        />
+        {title}
+      </button>
+      {isOpen && (
+        <div className="absolute z-10 bg-red rounded">
+          {/* eslint-disable-next-line react/prop-types */}
+          {items.map((item) => (
+            <div
+              key={item.path}
+              className="p-2 cursor-pointer hover:bg-red-500 hover:text-white"
+              onClick={() => handleItemClick(item.path)}
+              onMouseOver={() => setIsOpen(true)}
+              onMouseLeave={() => setIsOpen(false)}
             >
-                <Image
-                    src={icon}
-                    alt={title + " Icon"}
-                    className="lg:w-[40px] lg:h-[40px] w-[20px] h-[20px]"/>
-                {title}
-            </button>
-            {isOpen && (
-                <div className="absolute z-10 bg-red rounded">
-                    {items.map((item) => (
-                        <div
-                            key={item.path}
-                            className="p-2 cursor-pointer hover:bg-red-500 hover:text-white"
-                            onClick={() => handleItemClick(item.path)}
-                            onMouseOver={() => setIsOpen(true)}
-                            onMouseLeave={() => setIsOpen(false)}
-                        >
-                            {item.label}
-                        </div>
-                    ))}
-                </div>
-            )}
+              {item.label}
+            </div>
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 export default Dropdown;
-
