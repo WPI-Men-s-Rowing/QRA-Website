4d6191c31b99106b0f2801966ef9f2fabba93f9a
Emerson Shatouhy <emersont00>
2024-08-08 17:45:18 -0400

    Adds shadCN and fixes everything it broke(I think)

--- packages/site/src/components/results/RaceCard.tsx
+++ packages/site/src/components/results/RaceCard.tsx
@@ -33,8 +33,8 @@ interface RaceCardProps {
 function RaceCard(props: RaceCardProps) {
   return (
     <Link href={"/regattas/results/" + props.uuid}>
-      <div className="w-[379px] h-[100px] p-[15px] bg-neutral-50 rounded-[20px] border border-zinc-800 border-opacity-20 flex-col justify-start items-start gap-[5px] inline-flex">
-        <div className="text-zinc-800 text-opacity-50 text-[10px] font-normal capitalize">
+      <div className="w-[379px] h-auto p-[15px] bg-neutral-50 rounded-[20px] border border-zinc-800 border-opacity-20 flex-col justify-start items-start gap-[5px] inline-flex">
+        <div className="text-zinc-800 text-opacity-50 text-base font-normal capitalize">
           {props.type}
         </div>
         <div className="self-stretch text-text-color text-2xl font-bold">
